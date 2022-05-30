6130c847302b3aea407b043332004092557cfba6
Michael Stanley <michstan96@gmail.com>
2022-05-30 11:20:14 +0700

    Add data to send on HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        return view('home', [
+            'books' => Book::with('detail')->paginate(5),
+            'categories' => Category::all(),
+        ]);
     }
 }
