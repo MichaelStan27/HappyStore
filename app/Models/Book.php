18b277b1526b75296ad53826ee583fdf06722d84
Michael Stanley <michstan96@gmail.com>
2022-05-30 09:53:44 +0700

    add eloquent relationship to book and category models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Detail;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
@@ -12,4 +13,9 @@ class Book extends Model
     protected $guarded = [
         'id',
     ];
+
+    public function detail()
+    {
+        return $this->hasOne(Detail::class);
+    }
 }
