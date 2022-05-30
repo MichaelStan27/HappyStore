f38cb02bb2b48537c95c093d967fecd54730a9e8
Michael Stanley <michstan96@gmail.com>
2022-05-30 11:32:00 +0700

    Fix wrong name on navbar and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="bg-black text-white w-100 position-fixed bottom-0 py-2">
-    <h1 class="text-center fs-6">&copy Happy Store Book</h1>
+    <h1 class="text-center fs-6">&copy Happy Book Store</h1>
 </div>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/">Happy Store Book</a>
+      <a class="navbar-brand" href="/">Happy Book Store</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
