24c7658768031bafc14f01219c4819484f53d2fe
Michael Stanley <michstan96@gmail.com>
2022-05-30 11:20:51 +0700

    Fill content on home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,32 @@
 @section('title','Home')
 
 @section('content')
-
+    <div class="container mx-auto mt-4 d-flex gap-5">
+        <div class="w-75">
+            <h1 class="bg-dark text-light fs-2 px-2 py-2">Book List</h1>
+            <table class="table table-dark table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Author</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($books as $book)
+                        <tr>
+                            <td>{{ $book->title }}</td>
+                            <td>{{ $book->detail->author }}</td>
+                        </tr> 
+                    @endforeach
+                </tbody>
+              </table>
+              {{ $books->links() }}
+        </div>
+        <div class="">
+            <h1 class="bg-dark text-light fs-2 px-4 py-2">Category</h1>
+            @foreach ($categories as $category)
+                <a href="" class="d-block bg-black text-light text-center py-2">{{ $category->category }}</a>
+            @endforeach
+        </div>
+    </div>
 @endsection
